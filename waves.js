280b7a3b837fc6ed09ee0578ffe140fe24accda5
davide <dcarboni@gmail.com>
2020-12-27 17:12:53 +0100

    grid

---
+++ waves.js
@@ -0,0 +1,19 @@
+const ELEMENT_SIZE = 40;
+const GRID_SIZE = 80;
+
+var paintElement= function (x,y,c){
+    c.fillStyle='#000000';
+    c.fillRect(x*ELEMENT_SIZE,y*ELEMENT_SIZE,ELEMENT_SIZE,ELEMENT_SIZE);
+    c.strokeStyle='#ffffff';
+    c.strokeRect(x*ELEMENT_SIZE,y*ELEMENT_SIZE,ELEMENT_SIZE,ELEMENT_SIZE);
+}
+var main = function(canvas){
+    var c = canvas.getContext('2d');
+    for(var x=0;x<GRID_SIZE;x++)
+        for(var y=0;y<GRID_SIZE;y++)
+            paintElement(x,y,c);
+}
+
+
+
+
