e8b14119e215b566d2d239361cdc1c409c0c6aac
davide <dcarboni@gmail.com>
2020-12-28 18:16:20 +0100

    push and pop added

--- waves.js
+++ waves.js
@@ -2,24 +2,31 @@ const ELEMENT_SIZE = 40;
 const GRID_SIZE = 4;
 var theTime=0;
 
-var states=Array(GRID_SIZE * GRID_SIZE).fill(40);
-var tmpStates=Array(GRID_SIZE * GRID_SIZE);
+var states=Array(GRID_SIZE * GRID_SIZE);
 
 
 var index = (x,y) => x*GRID_SIZE + y;
-
- 
-
+var state  = (x,y) => states[index(x,y)] === undefined ? Array(0) : states[index(x,y)];
+var energy = (x,y) => state(x,y).length;
+var pop = function(x,y){
+    return state(x,y).pop();
+}
+var push = function(x,y,val){
+    if (energy(x,y) == 0) states[index(x,y)] = Array(0);
+    return states[index(x,y)].push(val);
+}
 //contour state init
-states[index(1,1)] = 120;
+states[index(1,1)] = Array(120).fill(1);
 
 var paintElement= function (x,y,c){
-    let energy = states[index(x,y)];
+    
 
-    c.fillStyle='rgb('+energy+',0,0)';
+    c.fillStyle='rgb('+energy(x,y)+',0,0)';
     c.fillRect(x*ELEMENT_SIZE,y*ELEMENT_SIZE,ELEMENT_SIZE,ELEMENT_SIZE);
     c.strokeStyle='#ffffff';
     c.strokeRect(x*ELEMENT_SIZE,y*ELEMENT_SIZE,ELEMENT_SIZE,ELEMENT_SIZE);
+    c.fillStyle = "#FFFFFF";
+    c.fillText(energy(x,y), x*ELEMENT_SIZE+5,y*ELEMENT_SIZE+10);
 }
 
 
@@ -34,15 +41,17 @@ var update = function(){
     var el = document.getElementById("theTime");
     el.innerHTML=theTime+"";
 
-    tmpStates[0]=2;
+    
     for(var x=0;x<GRID_SIZE;x++){
         for(var y=0;y<GRID_SIZE;y++){
-            tmpStates[index(x,y)] = states[index(x,y-1)]===undefined?0:states[index(x,y-1)];
+            if(energy(x,y)<energy(x-1,y)) push(x,y,pop(x-1,y));
+           // if(energy(x,y)<energy(x+1,y)) push(x,y,pop(x+1,y));
+            //if(energy(x,y)<energy(x,y+1)) push(x,y,pop(x,y+1));
+            //if(energy(x,y)<energy(x,y-1)) push(x,y,pop(x,y-1));
         }
     }
 
-    console.log(tmpStates);
-    states = tmpStates;
+    console.log(states);
     
 }
 
@@ -50,5 +59,5 @@ var update = function(){
 
 
 var main = function(canvas){    
-    setInterval(()=>{theTime++;update();repaint();},1000);
+    setInterval(()=>{theTime++;update(),repaint()},1000);
 }
