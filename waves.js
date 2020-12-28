9d3449a4a246117aa0f086d4d0a55c1b145084e0
davide <dcarboni@gmail.com>
2020-12-28 19:49:16 +0100

    removed the pop push logic

--- waves.js
+++ waves.js
@@ -1,27 +1,32 @@
-const ELEMENT_SIZE = 20;
-const GRID_SIZE = 30;
+const ELEMENT_SIZE = 40;
+const GRID_SIZE = 4;
 var theTime=0;
 
-var states=Array(GRID_SIZE * GRID_SIZE);
+var states=Array(GRID_SIZE * GRID_SIZE).fill(0);
 
 
 var index = (x,y) => x*GRID_SIZE + y;
-var state  = (x,y) => states[index(x,y)] === undefined ? Array(0) : states[index(x,y)];
-var energy = (x,y) => state(x,y).length;
-var pop = function(x,y){
-    return state(x,y).pop();
+var state  = (x,y) => states[index(x,y)];
+
+var incr = function(x,y,delta){
+    states[index(x,y)] += delta;
+    return delta;
 }
-var push = function(x,y,val){
-    if (energy(x,y) == 0) states[index(x,y)] = Array(0);
-    return states[index(x,y)].push(val);
+
+var decr = function(x,y,delta){
+    states[index(x,y)] -= delta;
+    return delta;
 }
+
 //contour state init
-states[index(1,1)] = Array(70000).fill(1);
-states[index(10,10)] = Array(50000).fill(1);
+states[index(1,1)] = 10;
+states[index(3,3)] = 50000;
+
+
 
 var paintElement= function (x,y,c){
     
-    var val = energy(x,y);
+    var val = state(x,y);
     let R=parseInt(val >= 256*256 ? 128 + (val / (256*256*2)) : 0 );
     let G=parseInt(val< 256*256 && val>=256 ? 128 + (val / 512)  : 0);
     let B=parseInt(val<256 ? 50 + val/2 : 0);
@@ -31,7 +36,7 @@ var paintElement= function (x,y,c){
     c.strokeStyle='#ffffff';
     c.strokeRect(x*ELEMENT_SIZE,y*ELEMENT_SIZE,ELEMENT_SIZE,ELEMENT_SIZE);
     c.fillStyle = "#FFFFFF";
-    c.fillText(energy(x,y), x*ELEMENT_SIZE+5,y*ELEMENT_SIZE+10);
+    c.fillText(state(x,y), x*ELEMENT_SIZE+5,y*ELEMENT_SIZE+10);
 }
 
 
@@ -48,12 +53,14 @@ var update = function(){
     
     for(var x=0;x<GRID_SIZE;x++){
         for(var y=0;y<GRID_SIZE;y++){
-            var emit = Math.random()*energy(x,y) > 1;
+            var emit = Math.random()*state(x,y) > 1;
             var direction = [[1,0],[-1,0],[0,1],[0,-1]][parseInt(Math.random()*4)];
             if(emit) 
                 if(x+direction[0]>=0 && x+direction[0]<GRID_SIZE)
-                    if(y+direction[1]>=0 && y+direction[1]<GRID_SIZE)
-                        push(x+direction[0],y+direction[1],pop(x,y));
+                    if(y+direction[1]>=0 && y+direction[1]<GRID_SIZE){
+                        incr(x+direction[0],y+direction[1],decr(x,y,1));
+                    }
+                        
         }
     }
 
@@ -64,5 +71,5 @@ var update = function(){
 
 
 var main = function(canvas){    
-    setInterval(()=>{theTime++;update(),repaint()},1000);
+    setInterval(()=>{theTime++;update(),repaint()},100);
 }
