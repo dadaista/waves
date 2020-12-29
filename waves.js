b4e368a9915068ba02d21e104c0372a5a95f04ea
davide <dcarboni@gmail.com>
2020-12-29 18:51:53 +0100

    found better colors

--- waves.js
+++ waves.js
@@ -9,26 +9,57 @@ var index = (x,y) => x*GRID_SIZE + y;
 var state  = (x,y) => states[index(x,y)];
 
 var incr = function(x,y,delta){
-    states[index(x,y)] += delta;
+    if(!isDrain(x,y))
+        states[index(x,y)] += delta;
+
     return delta;
 }
 
 var decr = function(x,y,delta){
-    states[index(x,y)] -= delta;
+    if(!isGenerator(x,y))
+        states[index(x,y)] -= delta;
+
     return delta;
 }
 
+
+//we have a generator in 3,2
+var isGenerator = (x,y) => x == 3 && y == 2;
+
+//we have a drain in 2,4
+var isDrain = (x,y) => x == 2 && y == 4;
+
+
 //contour state init
-states[index(1,1)] = GRID_SIZE * GRID_SIZE;
+states[index(3,2)] = 8000;
 
 
+var heatMap = function(val){
+    if (val==0)   return  [0,0,0];
+    if (val<4)    return  [16,0,0];
+    if (val<8)    return  [32,0,0];
+    if (val<16)   return  [64,0,0];
+
+    if (val<64)     return  [0,64,0];
+    if (val<128)    return  [0,90,0];
+    if (val<256)    return  [0,128,0];
+    if (val<512)    return  [0,220,0];
+
+    if (val<1024)      return  
+    if (val<2048)      return  [0,64,64];
+    if (val<4096)      return  [0,128,128];
+    if (val<8192)      return  [0,255,255];    
+    return [255,255,255];
+
+}
 
 var paintElement= function (x,y,c){
     
     var val = state(x,y);
-    let R=parseInt(val * 255 / (GRID_SIZE*GRID_SIZE) );
-    let G=0;
-    let B=0;
+    let rgb = heatMap(val);
+    let R=rgb[0];
+    let G=rgb[1];
+    let B=rgb[2];
     
     c.fillStyle='rgb('+R+','+G+','+B+')';
     c.fillRect(x*ELEMENT_SIZE,y*ELEMENT_SIZE,ELEMENT_SIZE,ELEMENT_SIZE);
@@ -72,5 +103,5 @@ var update = function(){
 
 
 var main = function(canvas){    
-    setInterval(()=>{theTime++;update(),repaint()},100);
+    setInterval(()=>{theTime++;update(),repaint()},1000);
 }
